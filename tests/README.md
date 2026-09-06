b04f3b90a51b12b8d1599174fc027bb133920bda
Peter Li <peterli852@proton.me>
2026-09-06 20:26:14 +0800

    Create shell script for running unit tests

---
+++ tests/README.md
@@ -0,0 +1,7 @@
+# How to run tests
+
+## Run unit tests
+
+```bash
+tests/test_unit.sh 
+```
---
+++ tests/test_unit.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+docker compose --project-directory "${PROJECT_DIR}" -f "${PROJECT_DIR}/resources/ops/compose.yaml" -f "${PROJECT_DIR}/resources/ops/compose.dev.yaml" run --rm test /var/www/html/vendor/phpunit/phpunit/phpunit --bootstrap /var/www/html/tests/bootstrap.php --verbose /var/www/html/tests/unit
